025556461f5c1bbb3487e7c6db3a88721247612d
juanCruzCalderon <juancruzcalderoncontacto@gmail.com>
2023-01-14 16:38:06 -0300

    Logica formulario

--- script.js
+++ script.js
@@ -5,18 +5,27 @@ function getAll(cadena) {
 	return document.querySelectorAll(cadena);
 }
 
+
+//Funcion para que los inputs se dibujen en la card
 const num=getAll(".form__number input");
 const numCard=getAll(".back-card__numero");
 
 num.forEach((n,i)=>{n.addEventListener("blur",()=>{
-	if (n.value>=0 && n.value<=9999) 
-		{numCard[i].textContent=n.value;}
-	
+	if (n.value>=0 && n.value<=9999 && n.value!="") {
+		let buff="000"+n.value;
+		numCard[i].textContent=buff.slice(-4);}
+		else numCard[i].textContent="xxxx";
 })
 });
 
 
 const numeros=get(".card__number");
-
 console.log(num[0].index);
 
+
+//Boton para girar la tarjeta
+const capaGirar=get(".giracion");
+const btnGirar=get(".form__girar");
+btnGirar.addEventListener("click",(e)=>{
+	e.preventDefault();
+	capaGirar.classList.toggle("boton__girar")});
